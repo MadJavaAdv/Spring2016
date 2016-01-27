328e673b5554da633f5546fe47f09f28091431f5
Paula Waite <pawaite@madisoncollege.edu>
2016-01-27 16:14:35 -0600

    adding week2 day 2 files

---
+++ src/java112/demo/ReadDemo.java
@@ -0,0 +1,54 @@
+package java112.demo;
+
+import java.io.*;
+
+/** A sample class to demonstrate reading a file and related exception handling
+ *  @author Paula Waite
+ */
+
+public class ReadDemo {
+
+/** main method for starting the program
+ *  @param args the command line arguments
+ **/
+    public static void main(String[] args) {
+
+        ReadDemo demo = new ReadDemo();
+        demo.run();
+    }
+
+    public void run()  {
+        //TODO  research try with resources 
+        
+        BufferedReader in = null;
+
+        try {
+            FileReader reader = new FileReader("input.txt"); 
+            in = new BufferedReader(reader); 
+
+            while (in.ready()) {
+                System.out.println(in.readLine());
+            }    
+      
+        } catch (FileNotFoundException fileNotFoundException) {
+            System.out.println("There was a problem opening the file");
+            fileNotFoundException.printStackTrace();
+        } catch (IOException ioException) {
+            System.out.println("There was a problem reading the file");
+            ioException.printStackTrace();
+        }  catch (Exception exception) {
+            System.out.println("There was a really big problem");
+            exception.printStackTrace();
+        } finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException ioEx2) {
+                System.out.println("There was a problem closing the file");
+                ioEx2.printStackTrace();
+            }
+        }
+    }
+
+}
--- src/java112/labs1/LabThree.java
+++ src/java112/labs1/LabThree.java
@@ -3,11 +3,16 @@ package java112.labs1;
 public class LabThree {
 
     public static void main(String[] args) {
-        System.out.println("Please enter one argument on the command line");
-    }
+        LabThree lab = new LabThree();
+        if (args.length == 1) {
+            lab.run(args[0]);
+        } else {
+            System.out.println("Please enter one argument on the command line");
+        }    
+ }
 
     public void run(String input) {
-        System.out.println("input: test");
+        System.out.println("input: " + input);
     }
 
 }
